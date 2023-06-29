c4c044f55c3d92fd09f5abd075798b9df68e5eec
Jurrivh Leon <101796291+JurrivhLeon@users.noreply.github.com>
2023-06-29 20:10:45 +0800

    Update slbi_adam.py

--- slbi_adam.py
+++ slbi_adam.py
@@ -1,7 +1,7 @@
 """
 Neural Network and Deep Learning, Final Project.
 Optimization.
-Junyi Liao, 20307110289
+Junyi Liao
 An updated version of DessiLBI, with Adam.
 Modified from https://github.com/DessiLBI2020/DessiLBI/blob/master/DessiLBI/code/slbi_opt.py.
 """
